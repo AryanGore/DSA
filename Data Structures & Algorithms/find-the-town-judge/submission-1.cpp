8724e24192866b35e126968f44b69a0603fae8c9
AryanGore <162842363+AryanGore@users.noreply.github.com>
2026-08-05 09:52:41 +0530

    Add: find-the-town-judge - submission-1

---
+++ Data Structures & Algorithms/find-the-town-judge/submission-1.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int findJudge(int n, vector<vector<int>>& trust) {
+        unordered_map<int,vector<int>>trustlist;
+        vector<int>indegree(n+1,0);
+        vector<int>outdegree(n+1,0);
+
+        for(auto vec: trust){
+            trustlist[vec[0]].push_back(vec[1]);
+        }
+
+        for(auto per: trustlist){
+            outdegree[per.first] = per.second.size();
+        }
+
+        for(auto per: trustlist){
+            for(auto item: per.second){
+                indegree[item]++;
+            }
+        }
+
+        for(int i=1; i<n+1; ++i){
+            if(indegree[i] == n-1 && outdegree[i] == 0) return i;
+        }
+        return -1;
+    }
+};
